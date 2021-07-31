9605ea2a24f1e7e5367a8bafbe10bed51ee6c2c4
fux <jan.dick@bdp.org>
2021-07-31 16:46:01 +0200

    Adds possibilties to start from cmd

--- ibw.py
+++ ibw.py
@@ -1,12 +1,50 @@
+import json
 import os.path
-from matplotlib import pyplot as plt
 import numpy as np
+from pprint import pformat
+
+import click
+from matplotlib import pyplot as plt
 from igor.binarywave import load as loadibw
 
-import util
 
-def extract(filename):
-    data = loadibw(filename)
+def extract_data(path):
+    data = loadibw(path)
     wave = data['wave']
-    wData = np.nan_to_num(wave['wData']).tolist()
-    return wData
+    values = np.nan_to_num(wave['wData']).tolist()
+    del data['wave']['wData']
+    return (data, values)
+
+def store_data(path, data, values): 
+    path.replace('ibw', '')
+    path = path.replace('input', 'output')
+    lines = pformat(data).splitlines()
+    with open(f'{path}.txt', 'w') as writer:
+        writer.writelines([line + "\n" for line in lines])
+        # json.dump(data, writer, indent=4)
+    with open(f'{path}.json', 'w') as writer:
+        json.dump(values, writer, indent=4)
+
+def plot_data(values):
+    plt.plot(values)
+    plt.show()
+
+
+@click.command()
+@click.option('--path', help='specify the path and filename: "Data/<date>/<filename>"')
+@click.option('--plot', default=True, help='show plot')
+@click.option('--store', default=True, help='store data: stores complete data as txt and values as json.')
+def run(path, plot, store):
+    # Extract complete data and values 
+    data, values = extract_data(path)
+
+    # Store data:
+    if store:
+        store_data(path, data, values)
+
+    # Plot 
+    if plot:
+        plot_data(values)
+
+if __name__ == '__main__': 
+    run()
